9f716e0451b02fd14a428ada68a6d4d865cdef3f
Daria Senyaninova <daria.senyaninova@gmail.com>
2025-07-18 17:02:39 +0300

    page refactor (about us)

--- frontend/src/pages/AboutUsPage.jsx
+++ frontend/src/pages/AboutUsPage.jsx
@@ -1,5 +1,4 @@
 import Header from "../components/Header";
-import TextBlock from "../elements/TextBlock.jsx";
 import '../styles/about-page.css';
 
 
@@ -13,7 +12,7 @@ export default function AboutUsPage() {
 
                 <p className="about-paragraph">
                     At <span className="highlight">ConsultPRO Connect</span>, we provide expert consulting services
-                    to help businesses grow, optimize operations, and achieve long-term success!
+                    to help businesses grow, optimize operations, and achieve long-term success.
                 </p>
 
                 <p className="about-paragraph">
